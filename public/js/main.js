a422e3102c732385a3373d71c41ae1d830961af3
Jack Butler <guitar423@gmail.com>
2019-04-24 12:39:27 -0500

    added firebase files and index files

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,57 @@
+const firebase = require("firebase");
+
+var config = {
+	apiKey: "AIzaSyC7tYzNEgScBqWp9HgApxSkj4H6rSixx7c",
+	authDomain: "test-f56b4.firebaseapp.com",
+	databaseURL: "https://test-f56b4.firebaseio.com",
+	projectId: "test-f56b4",
+	storageBucket: "test-f56b4.appspot.com",
+	messagingSenderId: "677229509148"
+};
+firebase.initializeApp(config);
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    //User is signed in
-    document.getElementById("user-div").style.display = "block";
-    document.getElementById("login-div").style.display = "none";
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
   } else {
-    //User is not signed in
-    document.getElementById("user-div").style.display = "none";
-    document.getElementById("login-div").style.display = "block";
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
   }
 });
 
-function login() {
-  var userEmail = document.getElementById("email-field").value;
-  var userPass = document.getElementById("password-field").value;
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
 
   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
     // ...
   });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
 }
