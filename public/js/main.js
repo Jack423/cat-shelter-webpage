fb4b358c17a3289a2754e72c64d715cd3c7aa420
Jack Butler <guitar423@gmail.com>
2019-04-25 15:44:16 -0500

    Added snackbar for submission. Working on adding page for all cats.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var firebase = require('firebase');
+'use strict';
 
 var config = {
 	apiKey: "AIzaSyC7tYzNEgScBqWp9HgApxSkj4H6rSixx7c",
@@ -8,6 +8,9 @@ var config = {
 	storageBucket: "test-f56b4.appspot.com",
 	messagingSenderId: "677229509148"
 };
+
+const testButton = document.getElementById('test');
+
 firebase.initializeApp(config);
 
 firebase.auth().onAuthStateChanged(function(user) {
@@ -22,8 +25,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     if(user != null){
 
       var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
-
+      //document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
     }
 
   } else {
@@ -36,7 +38,6 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 function login(){
-
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
@@ -46,12 +47,89 @@ function login(){
     var errorMessage = error.message;
 
     window.alert("Error : " + errorMessage);
-
-    // ...
   });
 
 }
 
 function logout(){
   firebase.auth().signOut();
+}
+
+function addCat() {
+	var firestore = firebase.firestore();
+	var photoUrl = "";
+	var catImage = document.querySelector('#cat-image').files[0];
+	var name = document.getElementById('name');
+	var gender = document.getElementById('gender');
+	var breed = document.getElementById('breed');
+	var age = document.getElementById('age');
+	var description = document.getElementById('description');
+
+	var storageBucket = firebase.storage().ref(name.value);
+
+	var uploadTask = storageBucket.put(catImage);
+	// Register three observers:
+	// 1. 'state_changed' observer, called any time the state changes
+	// 2. Error observer, called on failure
+	// 3. Completion observer, called on successful completion
+	uploadTask.on('state_changed', function(snapshot){
+	  // Observe state change events such as progress, pause, and resume
+	  // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+	  var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+	  console.log('Upload is ' + progress + '% done');
+	  switch (snapshot.state) {
+	    case firebase.storage.TaskState.PAUSED: // or 'paused'
+	      console.log('Upload is paused');
+	      break;
+	    case firebase.storage.TaskState.RUNNING: // or 'running'
+	      console.log('Upload is running');
+	      break;
+	  }
+	}, function(error) {
+	  // Handle unsuccessful uploads
+	}, function() {
+	  // Handle successful uploads on complete
+	  uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+			console.log(downloadURL);
+			firestore.collection("cats").add({
+				name: name.value,
+				gender: gender.value,
+				breed: breed.value,
+				age: age.value,
+				description: description.value,
+				photoUrl: downloadURL
+			})
+			.then(function(docRef) {
+				console.log("Document is written with ID: ", docRef.id);
+			})
+			.catch(function(error) {
+				console.error("Error adding document: ", docRef);
+			});
+
+			// Get the snackbar DIV
+			var x = document.getElementById("snackbar")
+			// Add the "show" class to DIV
+			x.className = "show";
+			// After 3 seconds, remove the show class from DIV
+			setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+
+			// Clear values in the text boxes
+			name.value = "";
+			gender.value = "";
+			breed.value = "";
+			age.value = "";
+			description.value = "";
+			catImage.files[0] = null;
+	  });
+	});
+}
+
+function createCatElement(catId, name, gender, age, photoUrl) {
+	var uid = firebase.auth().currentUser.uid;
+	var html =
+		'<div class="card">' +
+			'<img id="photo" src="">' +
+			'<h6 id="name"></h6>' +
+			'<p id="gender"></p>' +
+		'</div>';
 }
--- public/js/test.js
+++
@@ -1,7 +0,0 @@
-var ui = new firebase.auth.AuthUI(firebase.auth());
-
-ui.start('#firebaseui-auth-container', {
-	signInOptions: [
-		firebase.auth.EmailAuthProvider.PROVIDER_ID
-	],
-});
