9012feaf1ad09b20718fcd6fa1d7a0bc87c854dd
Jack Butler <guitar423@gmail.com>
2019-04-30 01:39:12 -0500

    Updated main.js

--- public/js/main.js
+++ public/js/main.js
@@ -30,10 +30,8 @@ firebase.auth().onAuthStateChanged(function(user) {
 
   } else {
     // No user is signed in.
-
     document.getElementById("user_div").style.display = "none";
     document.getElementById("login_div").style.display = "block";
-
   }
 });
 
